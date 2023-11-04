147ec9319040afb743b114a02105304415bfa443
Vinay U <72002482+uvinay5@users.noreply.github.com>
2023-11-04 12:31:35 +0530

    Added AOS and Animations
    Added AOS and Animations

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-console.log("Done");
-$(document).ready(function () {
-    var movementStrength = 35;
-    var height = movementStrength / $(window).height();
-    var width = movementStrength / $(window).width();
-    $(".section-hero").mousemove(function (e) {
-        var pageX = e.pageX - $(window).width() / 2;
-        var pageY = e.pageY - $(window).height() / 2;
-        var newvalueX = width * pageX * -1 - 1;
-        var newvalueY = height * pageY * -1 - 1;
-        $(".section-hero").css("background-position", newvalueX + "px     " + newvalueY + "px");
-    });
+var movementStrength = 25;
+var height = movementStrength / window.screen.height;
+var width = movementStrength / window.screen.width;
+const hero = document.getElementById("section-hero");
+
+hero.addEventListener("mousemove", (e) => {
+    var pageX = e.clientX - window.screen.width / 2;
+    var pageY = e.clientY - window.screen.height / 2;
+    var newvalueX = width * pageX * -1 - 10;
+    var newvalueY = height * pageY * -1 - 0;
+    hero.style.backgroundPosition = newvalueX + "px     " + newvalueY + "px";
+
+    const div = document.createElement("div");
+    div.classList.add("hero-section-mouseClick");
+    div.style.left = e.clientX + "px";
+    div.style.top = e.clientY + "px";
+    const box = document.getElementById("section-hero");
+    box.appendChild(div);
+    setTimeout(() => {
+        div.remove();
+        box.removeChild(div);
+    }, 250);
 });
