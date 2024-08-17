0836bb8b5edc0f47e995e42c9b028246ca9dbadf
Vinay U <vinayu@portqii.com>
2024-08-17 22:35:04 +0530

    Offer model added

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 var movementStrength = 25;
-var height = movementStrength / window.screen.height;
-var width = movementStrength / window.screen.width;
+
 const hero = document.getElementById("section-hero");
 
-hero.addEventListener("mousemove", (e) => {
-    var pageX = e.clientX - window.screen.width / 2;
-    var pageY = e.clientY - window.screen.height / 2;
-    var newvalueX = width * pageX * -1 - 10;
-    var newvalueY = height * pageY * -1 - 0;
-    hero.style.backgroundPosition = newvalueX + "px     " + newvalueY + "px";
+var modal = document.getElementById("offerModal");
 
-    const div = document.createElement("div");
-    div.classList.add("hero-section-mouseClick");
-    div.style.left = e.clientX + "px";
-    div.style.top = e.clientY + "px";
-    const box = document.getElementById("section-hero");
-    box.appendChild(div);
-    setTimeout(() => {
-        box.removeChild(div);
-        div.remove();
-    }, 250);
+var closeButton = document.querySelector(".close-button");
+
+closeButton.onclick = function () {
+  modal.style.display = "none";
+};
+
+hero.addEventListener("mousemove", (e) => {
+  const div = document.createElement("div");
+  div.classList.add("hero-section-mouseClick");
+  div.style.left = e.pageX + "px";
+  div.style.top = e.pageY + "px";
+  const box = document.getElementById("section-hero");
+  box.appendChild(div);
+  setTimeout(() => {
+    box.removeChild(div);
+    div.remove();
+  }, 200);
 });
